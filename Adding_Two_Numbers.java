6f3e374bfaabc160d4edba624a7a97bae6d8ac0f
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-07 11:55:35 +0530

    import java.util.*; class Navya {     public static void main(String[] args)     {         Scanner anu=new Scanner(System.in);         int a,b,c;         a=anu.nextInt();         b=anu.nextInt();         c=a+b;         System.out.print(c);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Navya
+{
+    public static void main(String[] args)
+    {
+        Scanner anu=new Scanner(System.in);
+        int a,b,c;
+        a=anu.nextInt();
+        b=anu.nextInt();
+        c=a+b;
+        System.out.print(c);
+    }
+}
