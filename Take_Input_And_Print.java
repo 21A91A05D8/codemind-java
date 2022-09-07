7ca331d97399e8620497bb5821795da594c92201
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-07 19:58:15 +0530

    import java.util.*; class Navya {     public static void main(String[] args)     {         Scanner anu=new Scanner(System.in);         String s;         s=anu.nextLine();         System.out.println(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Navya
+{
+    public static void main(String[] args)
+    {
+        Scanner anu=new Scanner(System.in);
+        String s;
+        s=anu.nextLine();
+        System.out.println(s);
+    }
+}
