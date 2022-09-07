ea488ae87e9128a940b29af3e6728a3e5bc025b8
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-07 15:42:34 +0530

    import java.util.*; class Navya {     public static void main(String[] args)     {         Scanner anu=new Scanner(System.in);         int a,b,c;         a=anu.nextInt();         b=anu.nextInt();         c=a/b;         System.out.println(c);
        }
    }

---
+++ Division.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Navya
+{
+    public static void main(String[] args)
+    {
+        Scanner anu=new Scanner(System.in);
+        int a,b,c;
+        a=anu.nextInt();
+        b=anu.nextInt();
+        c=a/b;
+        System.out.println(c);
+        
+    }
+}
