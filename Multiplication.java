5b4119acd912ed33720acbff8b6320ac22b6a8c3
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-07 13:57:10 +0530

    import java.util.*; class anu {     public static void main(String[] args)     {         Scanner papa=new Scanner(System.in);         int a,b,c;         a=papa.nextInt();         b=papa.nextInt();         c=a*b;         System.out.print(c);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class anu
+{
+    public static void main(String[] args)
+    {
+        Scanner papa=new Scanner(System.in);
+        int a,b,c;
+        a=papa.nextInt();
+        b=papa.nextInt();
+        c=a*b;
+        System.out.print(c);
+    }
+}
