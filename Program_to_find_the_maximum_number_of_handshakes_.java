cba6d93c89cad14763dede338964be410b17952e
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-08 20:46:05 +0530

    import java.util.Scanner; class Navya {     public static void main(String args[])     {         Scanner anu=new Scanner(System.in);         int a=anu.nextInt();         int ans=(a*(a-1)/2);         System.out.println(ans);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Navya {
+    public static void main(String args[])
+    {
+        Scanner anu=new Scanner(System.in);
+        int a=anu.nextInt();
+        int ans=(a*(a-1)/2);
+        System.out.println(ans);
+    }
+}
+
