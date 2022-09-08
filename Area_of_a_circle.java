f4cee2a0647cc557c9dadb4f09fc1848aff2c22e
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-08 20:16:48 +0530

    import java.util.*; class Navya{     public static void main(String[] args)     {         Scanner anu=new Scanner(System.in);         int r;         r=anu.nextInt();         double b=r*r*(3.14);         System.out.format("%.2f",b);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Navya{
+    public static void main(String[] args)
+    {
+        Scanner anu=new Scanner(System.in);
+        int r;
+        r=anu.nextInt();
+        double b=r*r*(3.14);
+        System.out.format("%.2f",b);
+    }
+}
