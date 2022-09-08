2186bf431e0fe3637a40bb63f29abd438d94f832
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-08 20:36:21 +0530

    import java.util.*; class Navya {     public static void main(String args[])     {         double a,b;         Scanner anu=new Scanner(System.in);         double c;         a=anu.nextDouble();         b=anu.nextDouble();         c=(a+b)/2;         System.out.format("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Navya
+{
+    public static void main(String args[])
+    {
+        double a,b;
+        Scanner anu=new Scanner(System.in);
+        double c;
+        a=anu.nextDouble();
+        b=anu.nextDouble();
+        c=(a+b)/2;
+        System.out.format("%.4f",c);
+    }
+}
