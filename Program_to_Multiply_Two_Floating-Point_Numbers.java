ab0954ddb5a82c35ca8876d6f3e2479a0cefdfe1
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-08 20:37:59 +0530

    import java.util.*; class Navya {     public static void main(String args[])     {         Scanner anu=new Scanner(System.in);         double a,b,c;         a=anu.nextDouble();         b=anu.nextDouble();         c=a*b;         System.out.format("%.2f",c);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Navya
+{
+    public static void main(String args[])
+    {
+        Scanner anu=new Scanner(System.in);
+        double a,b,c;
+        a=anu.nextDouble();
+        b=anu.nextDouble();
+        c=a*b;
+        System.out.format("%.2f",c);
+    }
+}
