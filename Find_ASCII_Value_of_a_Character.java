89806233ca12b23e1ca7bec051a99a6773a7a4e8
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-21 17:55:23 +0530

    import java.util.*; class Navya{     public static void main(String args[])     {         Scanner anu=new Scanner(System.in);         char s=anu.next().charAt(0);         int c=s;         System.out.format("%d",c);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Navya{
+    public static void main(String args[])
+    {
+        Scanner anu=new Scanner(System.in);
+        char s=anu.next().charAt(0);
+        int c=s;
+        System.out.format("%d",c);
+    }
+}
