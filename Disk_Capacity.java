a83b94d97f9031326939f34057638519f82a2b74
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-21 18:04:43 +0530

    import java.util.*; class Navya{     public static void main(String args[])     {         Scanner anu=new Scanner(System.in);         int a=anu.nextInt();         int b=anu.nextInt();         int c=anu.nextInt();         System.out.format("%d",a*b*c*512*2);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Navya{
+    public static void main(String args[])
+    {
+        Scanner anu=new Scanner(System.in);
+        int a=anu.nextInt();
+        int b=anu.nextInt();
+        int c=anu.nextInt();
+        System.out.format("%d",a*b*c*512*2);
+    }
+}
