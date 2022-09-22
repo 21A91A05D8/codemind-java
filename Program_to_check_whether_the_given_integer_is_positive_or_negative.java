3e2e7e1b2d6aa9dcef15444184f8270e79f28f8b
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-22 08:14:07 +0530

    import java.util.*; class Navya{     public static void main(String args[]){         Scanner anu=new Scanner(System.in);         int n=anu.nextInt();         if (n>0){             System.out.println("Positive Number");         }         else{             System.out.println("Negative Number");         }     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Navya{
+    public static void main(String args[]){
+        Scanner anu=new Scanner(System.in);
+        int n=anu.nextInt();
+        if (n>0){
+            System.out.println("Positive Number");
+        }
+        else{
+            System.out.println("Negative Number");
+        }
+    }
+}
