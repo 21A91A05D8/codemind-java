564c6a9c13691e6d967461bc45ef493464414ea9
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-09-30 07:41:05 +0530

    import java.util.*; class Navya{     public static void main(String args[]){         Scanner anu=new Scanner(System.in);         int n;         n=anu.nextInt();         System.out.format("%d",n*(n+1)/2);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Navya{
+    public static void main(String args[]){
+        Scanner anu=new Scanner(System.in);
+        int n;
+        n=anu.nextInt();
+        System.out.format("%d",n*(n+1)/2);
+    }
+}
